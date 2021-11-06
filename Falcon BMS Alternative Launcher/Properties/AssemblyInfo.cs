65b218e87972dbf80177e6337cd2bb874adf341f
Chihiro Belmont <chihirobelmo@gmail.com>
2021-11-06 22:52:19 +0900

    Try AutoUpdate

--- Falcon BMS Alternative Launcher/Properties/AssemblyInfo.cs
+++ Falcon BMS Alternative Launcher/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.2.0")]
-[assembly: AssemblyFileVersion("1.8.2.0")]
+[assembly: AssemblyVersion("1.9.0.0")]
+[assembly: AssemblyFileVersion("1.9.0.0")]
