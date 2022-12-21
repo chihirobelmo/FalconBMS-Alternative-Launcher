cda7450f33bb8524fd315f89fb1411b8ec9730c5
Daniel Tian <danieltian@users.noreply.github.com>
2022-12-21 06:39:17 -1000

    Don't clear category dropdown after assigning control (#79)

--- Falcon BMS Alternative Launcher/Windows/MainWindow.xaml.cs
+++ Falcon BMS Alternative Launcher/Windows/MainWindow.xaml.cs
@@ -266,7 +266,7 @@ namespace FalconBMS.Launcher.Windows
 
                 joyAssign_2_inGameAxis();
                 ResetAssgnWindow();
-                ResetJoystickColumn();
+                RefreshJoystickColumn();
             }
             catch (Exception ex)
             {
