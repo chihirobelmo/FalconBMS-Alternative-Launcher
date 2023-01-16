846e700a4894ffc683a9780ed2d609be10f4f7dd
chihirobelmo <chihirobelmo@gmail.com>
2023-01-17 00:07:30 +0900

    v2.3.0.5

--- Falcon BMS Alternative Launcher/Properties/AssemblyInfo.cs
+++ Falcon BMS Alternative Launcher/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.0.4")]
-[assembly: AssemblyFileVersion("2.3.0.4")]
+[assembly: AssemblyVersion("2.3.0.5")]
+[assembly: AssemblyFileVersion("2.3.0.5")]
