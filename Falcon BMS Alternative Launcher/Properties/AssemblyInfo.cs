bd2bcd78e0308f7aa419436b2705c4ef97c1a92e
arithex <arithex@users.noreply.github.com>
2023-10-28 22:45:48 -0700

    BUGFIX - gracefully handle registry permission errors .. code-cleanup to improve consistency of readonly vs readwrite registy access

--- Falcon BMS Alternative Launcher/Properties/AssemblyInfo.cs
+++ Falcon BMS Alternative Launcher/Properties/AssemblyInfo.cs
@@ -48,7 +48,5 @@ using System.Windows;
 //
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
-//[assembly: AssemblyVersion("2023.10.19.3")]
-//[assembly: AssemblyFileVersion("2023.10.19.3")]
-[assembly: AssemblyVersion("2.4.0.7")]
-[assembly: AssemblyFileVersion("2.4.0.7")]
+[assembly: AssemblyVersion("2.4.1.1")]
+[assembly: AssemblyFileVersion("2.4.1.1")]
